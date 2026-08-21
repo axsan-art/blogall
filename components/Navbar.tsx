399f890b039e6e3fcbefaf2ff94566e6d0d49c28
Axel <axsan95@hotmail.com>
2026-08-21 15:08:45 -0500

    feat: agregar navbar dinamico y cierre de sesion

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,44 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "@/lib/supabase";
+
 type NavbarProps = {
   nombre: string;
 };
 
 export default function Navbar({ nombre }: NavbarProps) {
+  const router = useRouter();
+  const [sesionActiva, setSesionActiva] = useState(false);
+
+  useEffect(() => {
+    async function comprobarSesion() {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      setSesionActiva(!!session);
+    }
+
+    comprobarSesion();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSesionActiva(!!session);
+      }
+    );
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  async function cerrarSesion() {
+    await supabase.auth.signOut();
+    router.push("/login");
+  }
+
   return (
     <nav className="bg-slate-900 text-white px-6 py-4">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
@@ -13,8 +49,18 @@ export default function Navbar({ nombre }: NavbarProps) {
         <div className="flex gap-6">
           <a href="/">Inicio</a>
           <a href="/explorar">Explorar</a>
-          <a href="/login">Iniciar sesión</a>
-          <a href="/register">Registrarse</a>
+
+          {sesionActiva ? (
+            <button className="cursor-pointer"
+            onClick={cerrarSesion}>
+              Cerrar sesión
+            </button>
+          ) : (
+            <>
+              <a href="/login">Iniciar sesión</a>
+              <a href="/register">Registrarse</a>
+            </>
+          )}
         </div>
       </div>
     </nav>
